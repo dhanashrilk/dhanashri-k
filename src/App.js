c9d3cd50373124c2f190a19daef76e8c32be473f
Dhanashri K <104819834+dhanashrilk@users.noreply.github.com>
2024-05-25 15:37:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import { BrowserRouter as Router } from "react-router-dom";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Education from "./components/Education";
+import Contact from "./components/contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
+      <Contact />
+      <Footer />
+    </Router>
   );
 }
 
